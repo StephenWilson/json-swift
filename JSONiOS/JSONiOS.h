4dc5b173e2bb6988c684d73d4fd788e309fc6cef
Stephen Wilson <stee@gravitywell.co.uk>
2014-07-30 17:26:36 +0100

    Added iOS JSON framework

---
+++ JSONiOS/JSONiOS.h
@@ -0,0 +1,19 @@
+//
+//  JSONiOS.h
+//  JSONiOS
+//
+//  Created by Stephen Wilson on 30/07/2014.
+//  Copyright (c) 2014 Kiad Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JSONiOS.
+FOUNDATION_EXPORT double JSONiOSVersionNumber;
+
+//! Project version string for JSONiOS.
+FOUNDATION_EXPORT const unsigned char JSONiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSONiOS/PublicHeader.h>
+
+
